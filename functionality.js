0d4b4483b544e9cf421fca1786933cb7e6a3ab5c
Matthew Sherwood <matthew.sherwood@student.sl.on.ca>
2023-04-19 16:25:36 -0400

    created the begining of the javascript backend

---
+++ functionality.js
@@ -0,0 +1,25 @@
+// table where data will be stored
+var responseBody = document.getElementById("body")
+// input varuables 
+var selectedWorkout = document.getElementById("workoutType")
+var selectedDifficulty = document.getElementById("difficulty")
+var selectedName = document.getElementById("nameOfWorkOut")
+
+window.onload = () =>{
+    // button id onclick functions
+    document.getElementById("getWorkoutType").onclick = getWorkoutType
+    document.getElementById("getDifficulty").onclick = getDifficulty
+    document.getElementById("getWorkoutName").onclick = getNameOfWorkout
+}
+
+function getWorkoutType() {
+
+}
+
+function getDifficulty(){
+
+}
+
+function getNameOfWorkout(){
+    
+}
